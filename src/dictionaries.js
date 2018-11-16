e22618ced0e32231195b3e209622685db7f5aaf8
cxdn0 <cxdname@gmail.com>
2018-11-16 14:54:38 +0200

    start hw8.3

---
+++ src/dictionaries.js
@@ -0,0 +1,19 @@
+export const en = {	
+	"close": "close",
+	"open": "open",
+	"update": "update",
+	"show comments": "show comments",
+	"delete me": "delete me",
+	"Loading": "Loading..."
+}
+
+export const ru {
+	"close": "закрыть",
+	"open": "открыть",
+	"update": "обновить",
+	"show comments": "показать комментарии",
+	"delete me": "удалить меня",
+	"Loading": "Загрузка..."
+}
+
+export default {en, ru}
